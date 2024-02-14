b6ba2d9d9918a8a3a4853d8e6248d3765ca0f1c9
Andrew Schroeder <aschroed@ualberta.ca>
2024-02-14 11:05:42 +0100

    Added ability to click on gridworld

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Button
 
 class Dipole:
     def __init__(self, state=0):
@@ -23,11 +25,19 @@ class Board:
 
     def display(self):
         states = self.get_states()
-        plt.imshow(states, cmap='binary', origin='lower', extent=[0, self.size_x, 0, self.size_y])
+
+        self.fig, self.ax = plt.subplots()
+
+        self.im = self.ax.imshow(self.get_states(), cmap='binary', origin='lower', extent=[0, self.size_x, 0, self.size_y])
         plt.grid(True, color='blue', linewidth=1)
         plt.xticks(np.arange(0, self.size_x + 1, 1))
         plt.yticks(np.arange(0, self.size_y + 1, 1))
         plt.gca().set_aspect('equal', adjustable='box')
+
+        self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+        self.propagate_button = Button(plt.axes([0.8, 0.01, 0.15, 0.05]), 'Propagate')
+        self.propagate_button.on_clicked(self.propagate_step)
+
         plt.show()
 
     def get_states(self):
@@ -42,9 +52,29 @@ class Board:
         # if np.random.rand() < self.flip_probability:
         #     self.covariant_flip(x, y)
 
-    # def covariant_flip(self, x, y):
-    #     # Perform covariant flips here
-    #     pass
+    def write_step(self, x, y):
+        self.grid[x, y].flip()
+        self.update_display()
+
+    def propagate_step(self, event):
+        # Calculate states of static dipoles based on dynamic dipoles and distance
+        # Update self.grid accordingly
+        pass
+
+    def read_step(self):
+        self.update_display()
+
+    def update_display(self):
+        self.im.set_array(self.get_states())
+        self.fig.canvas.draw()
+
+    def on_click(self, event):
+        if event.inaxes == self.ax:
+            x, y = int(event.xdata), int(event.ydata)
+
+            # For some reason the x and y coordinates need to be swapped for 
+            # clicking to work correctly. 
+            self.write_step(y, x)
 
 # Example usage
 board = Board(size_x=8, size_y=8, flip_probability=0.2)
